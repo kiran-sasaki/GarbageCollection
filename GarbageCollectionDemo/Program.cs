3a5afa531f6c7586cfe7e9de66d1666ae16a1e2a
kiran-sasaki <kiransekar1111@gmail.com>
2023-01-03 16:01:33 +0530

    [Kiran] Add: Garbage Collection

--- GarbageCollectionDemo/Program.cs
+++ GarbageCollectionDemo/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Garbage Collections");
+            Garbage.Collecting();
         }
     }
 }
